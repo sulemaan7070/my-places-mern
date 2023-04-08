82ddb05a8c5093f5cc2b250653b3d73143d54280
sulemaan7070 <sulemaan7070@gmail.com>
2023-04-08 14:20:52 +0530

    completed

---
+++ backend/app.js
@@ -0,0 +1,68 @@
+const fs = require("fs");
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use("/uploads/images", express.static(path.join("uploads", "images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+  next();
+});
+
+app.use("/api/places", placesRoutes);
+app.use("/api/users", usersRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
+
+app.use((error, req, res, next) => {
+  if (req.file) {
+    fs.unlink(req.file.path, (err) => {
+      console.log(err);
+    });
+  }
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
+app.listen(5000);
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(
+      `mongodb+srv://sulemaan7070:Sullu7070@cluster0.4uqy2fu.mongodb.net/?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      }
+    );
+    console.log(`Mongo db connected: ${conn.connection.host}`);
+
+    console.log(`listening on 5000`);
+  } catch (error) {
+    console.log(`Error is ${error.message}`);
+    process.exit();
+  }
+};
+connectDB();
---
+++ backend/util/location.js
@@ -0,0 +1,14 @@
+const axios = require("axios");
+
+const HttpError = require("../models/http-error");
+
+const API_KEY = "AIzaSyDgLmMpKCzveJf1_yuA0fUzzhy0WRChvZA";
+
+async function getCoordsForAddress(address) {
+  return {
+    lat: 40.7484474,
+    lng: -73.9871516,
+  };
+}
+
+module.exports = getCoordsForAddress;
